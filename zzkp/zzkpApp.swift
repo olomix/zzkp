271c9bfb439bb2af065d653aa107c4b12b0b4dcb
Oleg Lomaka <oleg.lomaka@gmail.com>
2024-10-23 14:02:24 -0400

    Initial Commit

---
+++ zzkp/zzkpApp.swift
@@ -0,0 +1,17 @@
+//
+//  zzkpApp.swift
+//  zzkp
+//
+//  Created by Oleg Lomaka on 23.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct zzkpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ zzkpTests/zzkpTests.swift
@@ -0,0 +1,17 @@
+//
+//  zzkpTests.swift
+//  zzkpTests
+//
+//  Created by Oleg Lomaka on 23.10.2024.
+//
+
+import Testing
+@testable import zzkp
+
+struct zzkpTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
